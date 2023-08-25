e577a3c75474333440463e3f33c8f8dbba0dfdfb
Xepobopa <ismartsdn4477@gmail.com>
2023-08-25 11:50:21 +0300

    Add an endpoint `/users/get` that returns user by token

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,6 +4,7 @@ import {
     Delete,
     Get,
     HttpStatus,
+    NestInterceptor,
     Param,
     ParseFilePipeBuilder,
     Patch,
@@ -35,12 +36,14 @@ import {Role} from "../role/role.enum";
 import {RolesGuard} from "../role/roles.guard";
 import {UpdateUserDto} from "../dto/update-user.dto";
 import {JwtUserGuard} from "../authorization/auth.guard";
+import {NotFoundInterceptor} from "../interceptor/not-found.interceptor";
 
 @ApiTags('User')
 @ApiBearerAuth("access-token")
 @ApiUnauthorizedResponse({
     description: "Your access token is not valid or expired."
 })
+@UseInterceptors(NotFoundInterceptor)
 @Controller('users')
 @UseGuards(JwtUserGuard)
 export class UsersController {
@@ -76,7 +79,7 @@ export class UsersController {
             },
         },
     })
-    @UseInterceptors(FileInterceptor('avatar'))
+    @UseInterceptors(FileInterceptor('avatar') as unknown as NestInterceptor)
     @Post('create')
     async create(
         @Body() newUser: CreateUserDto,
@@ -124,7 +127,7 @@ export class UsersController {
             },
         },
     })
-    @UseInterceptors(FileInterceptor('avatar'))
+    @UseInterceptors(FileInterceptor('avatar') as unknown as NestInterceptor)
     @Patch(':uuid/setAvatar')
     async changeAvatar(
         @Param('uuid') uuid: string,
@@ -149,8 +152,18 @@ export class UsersController {
     @ApiOkResponse({description: "User with provided 'uuid'.", type: UserDto})
     @ApiBadRequestResponse({description: "Can't find user with provided 'uuid'"})
     @Get('get/:uuid')
-    async getUser(@Param('uuid') uuid: string) {
-        return await this.usersService.getUser(uuid);
+    async getUserById(@Param('uuid') uuid: string) {
+        return await this.usersService.getUserByUuid(uuid);
+    }
+
+    @ApiOperation({summary: "Return a user by provided token"})
+    @ApiOkResponse({description: "User", type: UserDto})
+    @ApiBadRequestResponse({description: "Can't find user by provided token"})
+    @Get('get')
+    async getUserByToken(@Req() req: Request) {
+        const token = req.headers.authorization.replace('Bearer ', '');
+        const sub = this.jwtService.decode(token).sub;
+        return await this.usersService.getUserBySub(sub);
     }
 
     @ApiOperation({summary: "Delete user with provided 'uuid'"})
