989d8c5a82954f336b9d4625d58e5dea44f8dfe4
Xepobopa <ismartsdn4477@gmail.com>
2023-08-25 11:52:05 +0300

    migrate from aws to clever-cloud

--- src/upload/upload.service.ts
+++ src/upload/upload.service.ts
@@ -1,49 +1,41 @@
 import {ConsoleLogger, Injectable} from '@nestjs/common';
 import {ConfigService} from "@nestjs/config";
-import {DeleteObjectCommand, PutObjectCommand, S3Client} from "@aws-sdk/client-s3";
+import * as AWS from "aws-sdk";
 
 @Injectable()
 export class UploadService {
-    private readonly s3Client = new S3Client({
-        region: this.configService.get<string>('AWS_REGION'),
-        credentials: {
-            accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
-            secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
-        }
-    })
+    private s3Client: AWS.S3;
 
     constructor(private readonly configService: ConfigService) {
-        console.log("Start AWS S3Client")
+        this.s3Client = new AWS.S3({
+            endpoint: this.configService.get<string>('AWS_HOST'),
+            credentials: {
+                accessKeyId: this.configService.get<string>('AWS_ACCESS_KEY'),
+                secretAccessKey: this.configService.get<string>('AWS_SECRET_ACCESS_KEY'),
+            }
+        });
     }
 
     async uploadUser(filename: string, file: Buffer) {
-        await this.s3Client.send(
-            new PutObjectCommand({
-                Bucket: this.configService.get<string>('AWS_USER_BUCKET'),
-                Key: filename,
+        const res = await this.s3Client.upload(
+            {
+                Bucket: "omniforce-asset-store-user",
                 Body: file,
-            }),
-        );
-        return `https://${this.configService.get<string>('AWS_USER_BUCKET')}.s3.${this.configService.get<string>('AWS_REGION')}.amazonaws.com/${filename}`;
-    }
-
-    async deleteUser(filename: string) {
-        await this.s3Client.send(
-            new DeleteObjectCommand({
-                Bucket: this.configService.get<string>('AWS_USER_BUCKET'),
                 Key: filename,
-          })
-        );
+            }
+        )
+        res.send((err, data) => console.log(`Error: ${err}\n\nData: ${{...data}}`));
+        return `http://${this.configService.get<string>('AWS_HOST')}/${this.configService.get<string>('AWS_USER_BUCKET')}/${filename}`;
     }
 
     async uploadAsset(filename: string, file: Buffer) {
-        await this.s3Client.send(
-            new PutObjectCommand({
-                Bucket: this.configService.get<string>('AWS_ASSET_BUCKET'),
-                Key: filename,
+        await this.s3Client.upload(
+            {
+                Bucket: "omniforce-asset-store-asset",
                 Body: file,
-            }),
-        );
-        return `https://${this.configService.get<string>('AWS_ASSET_BUCKET')}.s3.${this.configService.get<string>('AWS_REGION')}.amazonaws.com/${filename}`;
+                Key: filename,
+            }
+        )
+        return `http://${this.configService.get<string>('AWS_HOST')}/${this.configService.get<string>('AWS_ASSET_BUCKET')}/${filename}`;
     }
 }
